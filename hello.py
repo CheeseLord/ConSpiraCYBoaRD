8a5032a402d8a6f9d67686d9c03b8021a2ff042c
Greg Kronmiller <kronmillerg@gmail.com>
2020-11-28 15:05:50 -0800

    Test commit please ignore.
    Change punctuation of hello world.

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ kivy.require('1.11.1')
 
 class MyApp(App):
     def build(self):
-        return Label(text='Hello world')
+        return Label(text='Hello, world!')
 
 
 if __name__ == '__main__':
