35f8681aa20e9dbff955771d4db12a0686e44052
Greg Kronmiller <kronmillerg@gmail.com>
2020-12-12 14:47:44 -0800

    Make a scatter with labels

--- src/main.py
+++ src/main.py
@@ -1,13 +1,19 @@
 import kivy
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen
 
 kivy.require('2.0.0')
 
 
+class FooScreen(Screen):
+    pass
+
 class MyApp(App):
     def build(self):
-        return Label(text='Hello, world!')
+        return Builder.load_file('data/test1.kv')
+        #return Label(text='Hello, world!')
 
 
 if __name__ == '__main__':
