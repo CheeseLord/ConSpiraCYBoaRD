2edef2228427494d7dd436be124da87d3365f1c4
Greg Kronmiller <kronmillerg@gmail.com>
2020-12-12 15:10:19 -0800

    Do this slightly less hackily

--- src/main.py
+++ src/main.py
@@ -2,20 +2,22 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
-from kivy.uix.screenmanager import Screen
+#from kivy.uix.screenmanager import Screen
+from kivy.uix.scatterlayout import ScatterLayout
 
 kivy.require('2.0.0')
 
 
-class FooScreen(Screen):
+class ConspiracyBoard(ScatterLayout):
     pass
 
-class MyApp(App):
+class ConspiracyBoardApp(App):
     def build(self):
-        return Builder.load_file('data/test1.kv')
+        # TODO set title in ConSpiraCYCaSE
+        return ConspiracyBoard()
         #return Label(text='Hello, world!')
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    ConspiracyBoardApp().run()
 
